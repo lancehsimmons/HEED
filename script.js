6aab0f050717544db26fd86b2ff1cd2fe207da78
Lance Simmons <lancehowardsimmons@gmail.com>
2021-10-01 16:19:31 -0400

    html boilerplate up w link and script. pc list for unpacking api added to js

---
+++ script.js
@@ -0,0 +1,24 @@
+// PSEUDO JS
+// 1 get the fetch working
+// 2 get the fetch in the terminal
+// 3 extract the "is_potentially_hazardous_asteroid": true/false
+// 4 extract miss_distance.miles
+// 5 extract relative_velocity.mile_per_hour
+// 6 extract "estimated_diameter_max"
+// 
+// 
+// 
+// 
+
+const button = document.querySelector('button')
+button.addEventListener('click', () => {
+  let title = document.getElementById('blank').value
+
+  fetch(`${BASE_URL}s=${title}`)
+    .then((res) => { return res.json() })
+
+    .then((resJSON) => {
+      console.log(resJSON)
+      renderList(resJSON.Search)
+    })
+})
