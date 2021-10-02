37d6a62d584e88ef749d672f087835bd0866ddc8
Lance Simmons <lancehowardsimmons@gmail.com>
2021-10-02 18:47:50 -0400

    got size and speed into arrays

--- script.js
+++ script.js
@@ -17,7 +17,6 @@ console.log ('we are connected')
 
 // A potentially hazardous object (PHO) is a near-Earth object whose orbit brings it within 4.7 million miles (7.5 million km) of Earth’s orbit, and is greater than 500 feet (140 meters) in size.
 
-
 // 
 // POST MVP
 // -asteroid impact animation
@@ -38,8 +37,10 @@ const BASE_URL = `${DOMAIN}?apikey=${API_KEY}&`;
 
 const missMiles = []
 const isHazardous = []
-const pho = []
-const nonPho = []
+const pho = [] /*array of miss distances*/
+const nonPho = [] /*array of miss distances*/
+const phoSizeAll = []
+const phoSpeedAll = []
 
 
 // fetch address should go like this: 
@@ -51,16 +52,13 @@ fetch('https://api.nasa.gov/neo/rest/v1/feed?2021-10-01=START_DATE&api_key=xeZDk
       // console.log(resJSON)
       // reference for object structure of neo object
       // console.log(resJSON.near_earth_objects)
-      // renderList(resJSON)
       // this one works to get the boolean back from first array in NEO object
-      // I think I might have to loop over n_e_o to get the actual date value objects so that the boolean can be programmatically extracted from the json
       // console.log(resJSON.near_earth_objects['2021-10-02'][0].is_potentially_hazardous_asteroid)
 
       let dateExtractor = resJSON.near_earth_objects
 
       let dates = []
       // this for loop generates an array of date objects. 
-      // then should be able to iterate PHO booleans and split dates into PHO and benign asteroid arrays
       for (var property in dateExtractor) {
         // console.log(property)
         dates.push(property)
@@ -72,11 +70,9 @@ fetch('https://api.nasa.gov/neo/rest/v1/feed?2021-10-01=START_DATE&api_key=xeZDk
         neoDay = dates[i]
         // this line logs the i_p_h_a boolean for the first asteroid of each date returned from the 7 day search results
         // console.log(dateExtractor[`${neoDay}`][0].is_potentially_hazardous_asteroid)
-        // logging the number of asteroids per day (one more than array length). hopefully just one more step to get a boolean for every asteroid
+        // logging the number of asteroids per day (one more than array length). 
         // console.log(dateExtractor[`${neoDay}`].length + 1)
-        // push all the booleans into an array
-        // check each using if/else
-        // use if/else func to either build divs or send object data to the appropriate build div function
+
 
         // on this line asteroidExtractor is returning the date plus the index of each asteroid
         asteroidExtractor(dateExtractor[`${neoDay}`])
@@ -105,13 +101,17 @@ const asteroidExtractor = (day) => {
   day.forEach(asteroid => {
     // console.log(asteroid)
     // console.log(asteroid.close_approach_data[0].miss_distance.miles)
-    // put distances in array for true and false
     // this line puts all of the miss miles in an array
     missMiles.push(asteroid.close_approach_data[0].miss_distance.miles)
 
     // this line puts all of the hazardous booleans in an array
     // console.log(asteroid.is_potentially_hazardous_asteroid)
     isHazardous.push(asteroid.is_potentially_hazardous_asteroid)
+
+    phoSizeAll.push(asteroid.estimated_diameter.feet.estimated_diameter_max)
+
+    phoSpeedAll.push(asteroid.close_approach_data[0].relative_velocity.miles_per_hour)
+
   })
   
 }
@@ -126,7 +126,7 @@ const warningSystem = (hazardous, nonhazardous) => {
     document.body.appendChild(warning)
 
     hazardous.forEach((asteroid) => {
-      console.log(asteroid)
+      // console.log(asteroid)
       const phoDiv = document.createElement('div')
       phoDiv.className = 'pho'
       phoDiv.innerText = `A near miss at only ${Math.round(asteroid)} miles`
@@ -142,20 +142,16 @@ const warningSystem = (hazardous, nonhazardous) => {
     nonphoDiv.innerText = asteroid
     document.body.appendChild(nonphoDiv)
   })
-
-  // hazardous.forEach(asteroid)
-  // const phoDiv = document.createElement('div')
-  // phoDiv.className = 'nonpho'
-  // phoDiv.innerText = hazardous[asteroid]
-  // document.body.aqppendChild(phoDiv)
   
 }
 
 
 // console.log(missMiles)
 // console.log(isHazardous)
-console.log(pho)
-console.log(nonPho)
+// console.log(pho)
+// console.log(nonPho)
+console.log(phoSizeAll)
+console.log(phoSpeedAll)
 
 
 
